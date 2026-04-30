ac24edcf96fe7ddc98c60eb6d3bec79359af4831
lunazkoe <lunazkoe@gmail.com>
2026-04-30 17:09:02 +0900

    [FINISH] S06_P

---
+++ S06_P/src/main/java/com/springboot/jpa/S06PApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S06PApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S06PApplication.class, args);
+    }
+
+}
---
+++ S06_P/src/test/java/com/springboot/jpa/S06PApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S06PApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
